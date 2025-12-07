f6cb052556a43ab0686bc2e522d6b4defa47d56d
Dinogorgon <P3yton.Decker@gmail.com>
2025-12-06 22:57:45 -0500

    Fix path alias resolution with webpack config and moduleResolution

--- next.config.js
+++ next.config.js
@@ -1,9 +1,18 @@
+const path = require('path')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
   images: {
     unoptimized: true,
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, '.'),
+    }
+    return config
   },
 }
 
